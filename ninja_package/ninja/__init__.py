2656f3c85279a71a7d26527070a217e3fd6db067
NINJAGAMING107 <89859411+NINJAGAMING107@users.noreply.github.com>
2024-08-01 11:31:58 +0530

    Add files via upload

---
+++ ninja_package/ninja/__init__.py
@@ -0,0 +1,3 @@
+# ninja/__init__.py
+
+from .ninja import display_custom_ninja
---
+++ ninja_package/ninja/ninja.py
@@ -0,0 +1,18 @@
+# ninja/ninja.py
+
+custom_ninja = """
+Ninja of Programming
+
+Consistency is key.
+Keep it simple, keep it clear.
+Reusability over redundancy.
+Code for humans first, machines second.
+Clarity over cleverness.
+Always be learning.
+"""
+
+def display_custom_ninja():
+    print(custom_ninja)
+
+# Automatically display the custom ninja text when the module is imported
+display_custom_ninja()
